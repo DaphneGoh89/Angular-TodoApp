a8edd5ece3c7d61761be9a4a9bb3990a7dd28672
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-06-10 16:16:28 +0800

    MVP completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroTrash,
+  heroCheck,
+  heroPencilSquare,
+} from '@ng-icons/heroicons/outline';
+import { ionSaveOutline } from '@ng-icons/ionicons';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgIconsModule.withIcons({
+      heroTrash,
+      heroCheck,
+      heroPencilSquare,
+      ionSaveOutline,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
